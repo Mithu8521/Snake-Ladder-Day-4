964c85ee4ae156c39921031753d6849408ae6f48
Mithu8521 <mithileshkr8521@gmail.com>
2021-08-23 15:21:33 +0530

    Welcome to snake ladder

---
+++ SinglePlayer.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class SinglePlayer {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Snake And Ladder Program");
+        int start = 0;
+        int Finish = 100;
+        int playerPos;
+
+        System.out.println("Game Start");
+        playerPos = start;
+        System.out.println("Player at Position" +playerPos);
+    }
+}
